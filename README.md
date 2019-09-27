958f41db347ca16e1724196606e89cc74feb6ec8
Elin Hagman <eliyetres@hotmail.com>
2019-09-27 14:07:40 +0200

    individualexception

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # Dialogue Systems 2
 
-## Lab 1: NLU for Dialogue Systems 
+## Lab 1: NLU for Dialogue Systems
 
 ### Resluts are in  [REPORT.md](https://github.com/eliyetres/NLU1/blob/master/Lab1/REPORT.md)
 
-
 ## Lab 2: Call John
+
+### An introduction to the Talkamatic Dialogue Manager (TDM) which is based on IBiS/GoDiS (Larsson 2002)
+
+## Lab 3: Lab 3: Data collection, distillation and annotation
+
+### Data collection tasks, including distillation and annotation with dialogue moves
+
+## Lab 4: A simple weather app
+
+### Build an app from scratch that access an online API, http://openweathermap.org/current
 
 ---
 
 # Assignment: Dialogue system evaluation
-
--- lab4/weather_lab4/weather/device.py
+++ lab4/weather_lab4/weather/device.py
@@ -1,34 +1,77 @@
-from tdm.lib.device import DddDevice, EntityRecognizer
+from tdm.lib.device import DddDevice, DeviceWHQuery
+from urllib2 import Request,urlopen 
+import json
 
+
+
+def getData(self,city,country):
+    key = "378ff1bfe6cbfe897ddddf2bf5dbd60d"
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=%s,%s&APPID=%s' % (city,country,key)
+    print(url)
+    request = Request(url)
+    response = urlopen(request)
+    data = response.read()
+    return json.loads(data)
 
 class WeatherDevice(DddDevice):
-    class WeatherRecognizer(EntityRecognizer):
-        """Entity recognizer for Weather"""
 
-        def recognize(self, utterance, language):
-            """Recognize entities in a user utterance, given the specified language.
+    LONDON = "city_london"
+    GOTHENBURG = "city_gothenburg"
 
-            This method is responsible for finding all dynamic entities in the utterance. Its accuracy affects the
-            behaviour of the dialogue system.
+    CITIES = {
+        "London":LONDON,
+        "Gothenburg": GOTHENBURG
+    }
 
-            Since the search is conducted during runtime, particular care should be taken to ensure that the method is
-            accurate, robust and has sufficient performance.
+    ENGLAND = "country_uk"
+    SWEDEN = "country_swe"
 
-            Args:
-                utterance (str): The utterance to be searched. For example 'call John'.
-                language  (str): The language code of the utterance according to the ISO 639-2/B standard.
-                                 Exceptions are Swedish ('sv' instead of 'swe') and Italian ('it' instead of 'ita').
+    TEMPERATURE = "temperature"
+    WEATHER = "weather"
 
-            Returns:
-                list of dicts: Given the example utterance "call John", the following entity could be returned
-                [
-                    {
-                        "sort": "contact",       # The sort must be declared in the ontology.
-                        "grammar_entry": "John", # The grammar entry as it occurred in 'utterance'.
-                        "name": "contact_john",  # [optional] Should be a globally unique identifier. Must never be
-                                                 # found as is in a user utterance. Use for example the form Sort_ID
-                                                 # (e.g. contact_john).
-                    },
-                ]
-            """
-            return []
+    class temperature(DeviceWHQuery):
+        def perform(self,city,country):
+           data = self.device.getData(city, country)
+           temp = data['main']['temp']
+           tempstr = str(temp)
+           return [tempstr]
+
+    class weather(DeviceWHQuery):
+        def perform(self,city,country):
+           data = self.device.getData(city, country)
+           we = data['weather']['description']
+           westr = str(we)
+           return [westr]
+
+
+# class WeatherDevice(DddDevice):
+#     class WeatherRecognizer(EntityRecognizer):
+#         """Entity recognizer for Weather"""
+
+#         def recognize(self, utterance, language):
+#             """Recognize entities in a user utterance, given the specified language.
+
+#             This method is responsible for finding all dynamic entities in the utterance. Its accuracy affects the
+#             behaviour of the dialogue system.
+
+#             Since the search is conducted during runtime, particular care should be taken to ensure that the method is
+#             accurate, robust and has sufficient performance.
+
+#             Args:
+#                 utterance (str): The utterance to be searched. For example 'call John'.
+#                 language  (str): The language code of the utterance according to the ISO 639-2/B standard.
+#                                  Exceptions are Swedish ('sv' instead of 'swe') and Italian ('it' instead of 'ita').
+
+#             Returns:
+#                 list of dicts: Given the example utterance "call John", the following entity could be returned
+#                 [
+#                     {
+#                         "sort": "contact",       # The sort must be declared in the ontology.
+#                         "grammar_entry": "John", # The grammar entry as it occurred in 'utterance'.
+#                         "name": "contact_john",  # [optional] Should be a globally unique identifier. Must never be
+#                                                  # found as is in a user utterance. Use for example the form Sort_ID
+#                                                  # (e.g. contact_john).
+#                     },
+#                 ]
+#             """
+#             return []
