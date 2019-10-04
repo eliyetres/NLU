e23ef9ab1741944e0965fac74538b96982c9065a
Elin Hagman <eliyetres@hotmail.com>
2019-10-04 13:20:25 +0200

    finished lab4

--- lab4/weather_lab4/weather/device.py
+++ lab4/weather_lab4/weather/device.py
@@ -2,76 +2,48 @@ from tdm.lib.device import DddDevice, DeviceWHQuery
 from urllib2 import Request,urlopen 
 import json
 
-
-
-def getData(self,city,country):
-    key = "378ff1bfe6cbfe897ddddf2bf5dbd60d"
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=%s,%s&APPID=%s' % (city,country,key)
-    print(url)
-    request = Request(url)
-    response = urlopen(request)
-    data = response.read()
-    return json.loads(data)
-
 class WeatherDevice(DddDevice):
 
-    LONDON = "city_london"
-    GOTHENBURG = "city_gothenburg"
+    metric = "metric"
+    imperial = "imperial"
 
-    CITIES = {
-        "London":LONDON,
-        "Gothenburg": GOTHENBURG
+    UNITS = {
+        "celsius": metric, 
+        "metric": metric, 
+        "fahrenheit": imperial, 
+        "imperial": imperial
     }
 
-    ENGLAND = "country_uk"
-    SWEDEN = "country_swe"
+    def getUnit(self, unit):
+        selected_unit = self.UNITS[unit]
+        return selected_unit
 
-    TEMPERATURE = "temperature"
-    WEATHER = "weather"
+    def getData(self, city, country, units="metric"):
+        key = "378ff1bfe6cbfe897ddddf2bf5dbd60d"
+        units = self.getUnit(units)
+        url = 'http://api.openweathermap.org/data/2.5/weather?q=%s,%s&units=%s&APPID=%s' % (city,country,units,key)
+        print(url)
+        request = Request(url)
+        response = urlopen(request)
+        data = response.read()
+        return json.loads(data)
 
     class temperature(DeviceWHQuery):
-        def perform(self,city,country):
-           data = self.device.getData(city, country)
-           temp = data['main']['temp']
-           tempstr = str(temp)
-           return [tempstr]
+        def perform(self, city, country, units="metric"):
+            units = self.device.getUnit(units)
+            data = self.device.getData(city, country, units)
+            temp = data['main']['temp']
+            tempstr = str(int(temp))
+            print("Temp: ", tempstr, "units:", units)
+            return [tempstr]
 
     class weather(DeviceWHQuery):
-        def perform(self,city,country):
+        def perform(self, city, country):
            data = self.device.getData(city, country)
-           we = data['weather']['description']
-           westr = str(we)
-           return [westr]
-
-
-# class WeatherDevice(DddDevice):
-#     class WeatherRecognizer(EntityRecognizer):
-#         """Entity recognizer for Weather"""
-
-#         def recognize(self, utterance, language):
-#             """Recognize entities in a user utterance, given the specified language.
-
-#             This method is responsible for finding all dynamic entities in the utterance. Its accuracy affects the
-#             behaviour of the dialogue system.
-
-#             Since the search is conducted during runtime, particular care should be taken to ensure that the method is
-#             accurate, robust and has sufficient performance.
-
-#             Args:
-#                 utterance (str): The utterance to be searched. For example 'call John'.
-#                 language  (str): The language code of the utterance according to the ISO 639-2/B standard.
-#                                  Exceptions are Swedish ('sv' instead of 'swe') and Italian ('it' instead of 'ita').
-
-#             Returns:
-#                 list of dicts: Given the example utterance "call John", the following entity could be returned
-#                 [
-#                     {
-#                         "sort": "contact",       # The sort must be declared in the ontology.
-#                         "grammar_entry": "John", # The grammar entry as it occurred in 'utterance'.
-#                         "name": "contact_john",  # [optional] Should be a globally unique identifier. Must never be
-#                                                  # found as is in a user utterance. Use for example the form Sort_ID
-#                                                  # (e.g. contact_john).
-#                     },
-#                 ]
-#             """
-#             return []
+           main_we = data['weather'][0]['main']
+           we = data['weather'][0]['description']
+           weather = str(main_we+" with "+we)
+           if main_we.lower() == we:
+               weather = main_we           
+           print(weather)
+           return [weather]
