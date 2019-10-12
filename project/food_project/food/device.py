5f739cf79140b81a11e02d80182b2d13632e0b79
eliyetres <eliyetres@hotmail.com>
2019-10-12 17:05:34 +0200

    changed grammar to only contain 3 limited slots for each item, imported request to send parameters to API, changed detailed recipe to return ingredients for first recipe, though without recipe title

--- project/food_project/food/device.py
+++ project/food_project/food/device.py
@@ -2,36 +2,31 @@ from tdm.lib.device import  DddDevice, DeviceWHQuery
 from urllib2 import Request,urlopen
 import json
 import requests
-#import spoonacular as sp
-
-
 
 class FoodDevice(DddDevice):
 
     key = "94ef1f7a2b9941a1a9a315b35a7d5f40"
-    def getRecipeData(self,query,cuisine=None,diet=None,meal_type=None,intolerances=None,exclude_ingredients=None):
-        url = "https://api.spoonacular.com/recipes/search?apiKey={}".format(self.device.key)
-        param = {
+    def getRecipeData(self,query,cuisine,diet,meal_type,intolerances):
+        print(query, cuisine,diet,meal_type,intolerances)
+        url = "https://api.spoonacular.com/recipes/search?apiKey={}".format(self.key)
+        params = {
             "query": query,
             "cuisine": cuisine,
             "diet": diet,
             "meal_type": meal_type,
-            "intolerances": intolerances,
-            "exclude_ingredients":exclude_ingredients          
+            "intolerances": intolerances       
         }
         # url = "https://api.spoonacular.com/recipes/search?apiKey={}query={}&cuisine={}&diet={}&meal_type={}&intolerances={}".format(self.device.key,query,cuisine,diet,meal_type,intolerances)
 
         print(url)
-        print(param)
-        response = requests.get(url, params=param)
-        #request = Request(url)
-        #response = urlopen(request)
-        data = response.read()
-        return json.loads(data)
-    
+        print(params)        
+        response = requests.get(url, params=params)
+        print(response.json)
+        return response.json()
+   
     
     def getIngredientsData(self,recipe_id):
-        url = "https://api.spoonacular.com/recipes/{}/?apiKey={}".format(recipe_id, self.device.key)
+        url = "https://api.spoonacular.com/recipes/{}/information?apiKey={}".format(recipe_id, self.key)
         print(url)
         request = Request(url)
         response = urlopen(request)
@@ -39,34 +34,51 @@ class FoodDevice(DddDevice):
         return json.loads(data)
 
     class detailed_recipe(DeviceWHQuery):
-        def perform(self,query,cuisine,diet,meal_type,intolerances,exclude_ingredients=None):
-            recipe_data = self.device.getRecipeData(query,cuisine,diet,meal_type,intolerances,exclude_ingredients=None)
-            first_recipe_id = recipe_data.results[0].id
-            #first_recipe = data.results[0]
-            first_recipe_title = recipe_data.results[0].title
+        def perform(self,query,cuisine,diet,meal_type,intolerances):
+            recipe_data = self.device.getRecipeData(query,cuisine,diet,meal_type,intolerances)          
+            # If response returns no recipes, return string
+            if len(recipe_data['results']) >= 0:
+                return ["Sorry, no recipes found."]
+
+            first_recipe = recipe_data['results'][0]
+            first_recipe_id = first_recipe['id']          
+            first_recipe_title = first_recipe['title']           
 
             ingredients_data = self.device.getIngredientsData(first_recipe_id)
             ingredients_list = []
-            ingredients= ingredients_data.extendedIngredients
-            for ing in ingredients:
-                print(ing)
-                ingredients_list.append(ing)
+            ingredients= ingredients_data['extendedIngredients']
+            for i in range(len(ingredients)):
+                name = ingredients[i]['name']
+                amount = str(int(ingredients[i]['amount']))
+                unit  = ingredients[i]['unit']
+                ing_str = amount+" "+unit+" "+name
+                print(ing_str)
+                ingredients_list.append(ing_str)
             ingredients_str = ', '.join(ingredients_list)
 
-            return [first_recipe_title. ingredients_str]
+            print("\n DETAILED RECIPE")
+            print("------------------")
+            print("FIRST RECIPE")
+            print("------------------------------------------")
+            print(first_recipe_title)
+            print(ingredients_str)
+
+            #return [first_recipe_title, ingredients_str]
+            return [ingredients_str]
 
     class simple_recipe(DeviceWHQuery):
-        def perform(self,query,cuisine=None,diet=None,meal_type=None,intolerances=None,exclude_ingredients=None):
-            recipe_data = self.device.getRecipeData(query,cuisine=None,diet=None,meal_type=None,intolerances=None,exclude_ingredients=None)
-            first_recipe_id = recipe_data.results[0].id
-            first_recipe_title = recipe_data.results[0].title
+        def perform(self,query,cuisine,diet,meal_type,intolerances):
+            recipe_data = self.device.getRecipeData(query,cuisine,diet,meal_type,intolerances)
+            # first_recipe_id = recipe_data.results[0].id
+            # first_recipe_title = recipe_data.results[0].title
 
-            ingredients_data = self.device.getIngredientsData(first_recipe_id)
-            ingredients_list = []
-            ingredients= ingredients_data.extendedIngredients
-            for ing in ingredients:
-                print(ing)
-                ingredients_list.append(ing)
-            ingredients_str = ', '.join(ingredients_list)
+            # ingredients_data = self.device.getIngredientsData(first_recipe_id)
+            # ingredients_list = []
+            # ingredients= ingredients_data.extendedIngredients
+            # for ing in ingredients:
+            #     print(ing)
+            #     ingredients_list.append(ing)
+            # ingredients_str = ', '.join(ingredients_list)
             
-            return [first_recipe_title. ingredients_str]
+            # return [first_recipe_title, ingredients_str]
+            return ["test"]
